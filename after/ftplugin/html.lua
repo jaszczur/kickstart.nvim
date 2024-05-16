27b94abfda17cc5ce683ed207c3b45a970e37e2b
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-05-16 20:40:40 +0200

    html - open in browser

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,3 +1,9 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
+
+vim.keymap.set('n', '<localleader>oo', ':!open %<CR>', { desc = '[O]pen in browser', buffer = true })
+
+require('which-key').register {
+  ['<localleader>o'] = { name = '[O]pen', _ = 'which_key_ignore' },
+}
