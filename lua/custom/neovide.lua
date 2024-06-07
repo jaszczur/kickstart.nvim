dc1382a74734b7ca8233fb9b8ce9c2f686b866f0
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-06-07 19:42:54 +0200

    set iosevka as gui font

--- lua/custom/neovide.lua
+++ lua/custom/neovide.lua
@@ -1,4 +1,5 @@
 -- Neovide configuration
 -- see https://neovide.dev/configuration.html for details
 
-vim.o.guifont = 'FiraCode Nerd Font:h14'
+-- vim.o.guifont = 'FiraCode Nerd Font:h14'
+vim.o.guifont = 'Iosevka Nerd Font:h16'
