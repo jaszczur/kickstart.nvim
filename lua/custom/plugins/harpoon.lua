7ee97309c57f0cf62a72738471251d9f22ace577
Piotr Jaszczyk <jaszczur@Piotrs-MacBook-Pro.local>
2024-06-17 17:17:57 +0200

    don't use telescope in harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -5,33 +5,15 @@ return {
   config = function()
     local harpoon = require 'harpoon'
     harpoon:setup {}
-    local conf = require('telescope.config').values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = require('telescope.finders').new_table {
-            results = file_paths,
-          },
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-        })
-        :find()
-    end
     local map = vim.keymap.set
 
     map('n', '<leader>ha', function()
       harpoon:list():add()
     end, { desc = '[H]arpoon [a]dd' })
 
-    map('n', '<leader>hl', function()
-      toggle_telescope(harpoon:list())
-    end, { desc = '[H]arpoon [l]ist' })
+    map('n', '<leader>hh', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = '[H]arpoon list' })
 
     map('n', '<leader>hq', function()
       harpoon:list():select(1)
