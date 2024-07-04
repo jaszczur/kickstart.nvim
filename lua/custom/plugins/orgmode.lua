dad75133f8d152c8a64d2c9cbc90468ce3fbba80
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2024-07-04 15:23:53 +0200

    refactor orgmode module

--- lua/custom/plugins/orgmode.lua
+++ lua/custom/plugins/orgmode.lua
@@ -1,17 +1,15 @@
-local M = { 'nvim-orgmode/orgmode' }
-
-M.event = 'VeryLazy'
-M.ft = { 'org' }
-M.config = function()
-  -- Setup orgmode
-  require('orgmode').setup {
-    org_agenda_files = {
-      '~/Sync/org/gtd/inbox.org',
-      '~/Sync/org/gtd/gtd.org',
-      '~/Sync/org/gtd/tickler.org',
-    },
-    org_default_notes_file = '~/Sync/org/gtd/inbox.org',
-  }
-end
-
-return M
+return {
+  'nvim-orgmode/orgmode',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup {
+      org_agenda_files = {
+        '~/Sync/org/gtd/inbox.org',
+        '~/Sync/org/gtd/gtd.org',
+        '~/Sync/org/gtd/tickler.org',
+      },
+      org_default_notes_file = '~/Sync/org/gtd/inbox.org',
+    }
+  end,
+}
